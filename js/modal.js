f7b83ee0ba6eaa39928f5ff02773e4a4cf83639f
Parker Nilson <parker.todd.nilson@gmail.com>
2023-10-28 14:44:02 -0600

    create modal show and hide logic

---
+++ js/modal.js
@@ -0,0 +1,9 @@
+function showModal(id) {
+    const modal = document.getElementById(id)
+    modal.showModal()
+}
+
+function hideModal(id) {
+    const modal = document.getElementById(id)
+    modal.close()
+}
