3c4c69219a8af7b1e13945897424fbc722c4cdaa
Parker Nilson <parker.todd.nilson@gmail.com>
2023-11-18 14:50:04 -0700

    implement user authentication

--- public/js/login.js
+++ public/js/login.js
@@ -5,18 +5,74 @@
  */
 async function login() {
     const username = document.getElementById('username-input').value
+    const password = document.getElementById('password-input').value
 
-    const currentUser = localStorage.getItem('currentUser')
-    if (currentUser !== username) localStorage.setItem('currentUser', username)
+    const response = await fetch('/api/auth/login', {
+        method: "post",
+        body: JSON.stringify({ username, password }),
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8'
+        }
+    })
 
-    try {
-        await fetch(`/api/${username}/login`, {
-            method: "POST"
-        })
-
+    if (response.ok) {
+        localStorage.setItem('currentUser', username)
         window.location.href = 'soundboards.html'
-    } catch(error) {
-        console.error(error)
+    }
+}
+
+async function createUser() {
+    const username = document.getElementById('username-input').value
+    const password = document.getElementById('password-input').value
+
+    const response = await fetch('/api/auth/create', {
+        method: "post",
+        body: JSON.stringify({ username, password }),
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8'
+        }
+    })
+
+    if (response.ok) {
+        localStorage.setItem('currentUser', username)
+        window.location.href = 'soundboards.html'
+    }
+}
+
+const displayUserInfo = () => {
+    const currentUser = localStorage.getItem('currentUser')
+    if (currentUser) {
+        const userDisplayInfoElem = document.getElementById('user-info-display')
+        userDisplayInfoElem.classList.remove("hidden")
+
+        const usernameInfoSlot = document.getElementById('username-info-slot')
+        usernameInfoSlot.innerText = `You are logged in as: ${currentUser}`
+    } else {
+        hideUserInfo()
+    }
+}
+
+const hideUserInfo = () => {
+    const userDisplayInfoElem = document.getElementById('user-info-display')
+    userDisplayInfoElem.classList.add("hidden")
+
+    const usernameInfoSlot = document.getElementById('username-info-slot')
+    usernameInfoSlot.innerText = ''
+}
+
+const logout = async () => {
+    localStorage.removeItem('currentUser')
+    hideUserInfo()
+    await fetch(`/api/auth/logout`, {
+        method: "delete"
+    }).then(() => (window.location.href = '/'))
+}
+
+const getUser = async (username) => {
+    const response = await fetch(`/api/user/${username}`)
+    if (response.status === 200) {
+        return response.json()
     }
 
+    return null
 }
