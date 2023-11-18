f9bcd26e54a819652d4758c4931311a66658d9d5
Parker Nilson <parker.todd.nilson@gmail.com>
2023-11-18 14:49:17 -0700

    set up user auth functions

--- database.js
+++ database.js
@@ -1,5 +1,8 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const path = require('path')
+const bcrypt = require('bcrypt')
+const { v4: uuid } = require('uuid')
+const { getDefaultUserInfo } = require('./public/backend/utils')
 
 const credentials = path.resolve(__dirname, 'keys/mongodb-key.pem')
 const client = new MongoClient('mongodb+srv://cs260-startup.v7mhvyu.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority', {
@@ -22,6 +25,28 @@ const getDB = async () => {
     return db
 }
 
+const getUser = async (username) => await db.collection('users').findOne({ username })
+
+const getUserByToken = async (token) => await db.collection('users').findOne({ token })
+
+const createUser = async (username, password) => {
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const user = {
+        username,
+        password: passwordHash,
+        token: uuid(),
+        ...getDefaultUserInfo()
+    }
+
+    await db.collection('users').insertOne(user)
+
+    return user
+}
+
 module.exports = {
-    getDB
+    getDB,
+    getUser,
+    getUserByToken,
+    createUser
 }
